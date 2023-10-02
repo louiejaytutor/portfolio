1a8567e2e455398a73583cb1edadb4acd0c7b7cc
Louie Jay Tutor <109414917+louiejaytutor@users.noreply.github.com>
2023-10-02 16:47:39 +0800

    Add files via upload

--- js/responsive.js
+++ js/responsive.js
@@ -6,29 +6,41 @@ $(document).ready(function () {
             $(".container").removeClass("w-w1000");
             $(".container").addClass("w-full");
 
-            $(".container-l").removeClass("w-w500");
-            $(".container-l").addClass("w-full");
+            $(".about-l").removeClass("w-w450");
+            $(".about-l").addClass("w-full");
 
-            $(".container-r").removeClass("w-w450");
-            $(".container-r").addClass("w-full");
+            $(".about-r").removeClass("w-w450");
+            $(".about-r").addClass("w-full");
 
             $(".about-mypic").addClass("mb-10");
             $(".about-mypic").removeClass("h-h400");
             $(".about-mypic").addClass("h-h300");
+
+            $(".contact-l").removeClass("w-w500");
+            $(".contact-l").addClass("w-full");
+
+            $(".contact-r").removeClass("w-w450");
+            $(".contact-r").addClass("w-full");
         }
         else {
             $(".container").removeClass("w-full");
             $(".container").addClass("w-w1000");
 
-            $(".container-l").removeClass("w-full");
-            $(".container-l").addClass("w-w500");
+            $(".about-l").removeClass("w-full");
+            $(".about-l").addClass("w-w450");
 
-            $(".container-r").removeClass("w-full");
-            $(".container-r").addClass("w-w450");
+            $(".about-r").removeClass("w-full");
+            $(".about-r").addClass("w-w450");
 
             $(".about-mypic").removeClass("mb-10");
             $(".about-mypic").removeClass("h-h300");
             $(".about-mypic").addClass("h-h400");
+
+            $(".contact-l").removeClass("w-full");
+            $(".contact-l").addClass("w-w500");
+
+            $(".contact-r").removeClass("w-full");
+            $(".contact-r").addClass("w-w450");
         }
 
         if (windowWidth <= 767) {
