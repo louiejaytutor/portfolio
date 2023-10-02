197f892ae5a5cdf5445915b86341e4ad05572158
Louie Jay Tutor <109414917+louiejaytutor@users.noreply.github.com>
2023-10-02 12:48:35 +0800

    Add files via upload

---
+++ js/prog-lang.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".prog-lang").mouseenter(function () {
+        var title = $(this).attr("title");
+        var width = $(this).data("value");
+        
+        $("#percentage-bar").css("width", width);
+        $("#percentage-bar").removeClass("bg-clightblack");
+        $("#percentage-bar").addClass("bg-cblue");
+        $("#percentage").html(title + " (" + width + ")");
+    });
+    $(".prog-lang").mouseleave(function () {
+        $("#percentage-bar").css("width", "100%");
+        $("#percentage-bar").removeClass("bg-cblue");
+        $("#percentage-bar").addClass("bg-clightblack");
+        $("#percentage").html("Hover below");
+    });
+});
