fca38b5f275f2dd01d25cea8c3603f0baf7d94cb
Louie Jay Tutor <109414917+louiejaytutor@users.noreply.github.com>
2023-10-13 15:38:43 +0800

    Update tailwind.config.js

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -16,7 +16,7 @@ tailwind.config = {
                 "fs70": "70px",
             },
             fontFamily: {
-                 "bahnschrift": ["Bahnschrift", "sans"],
+                 sans: ["Bahnschrift", "Arial", "sans-serif"],
             },
             height: {
                 "h30": "30px",
